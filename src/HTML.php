ec52de0d27c119cbe934f51d18b43b0a92a3c081
Paulo Leonardo <39014913+paulo-leo@users.noreply.github.com>
2024-04-24 16:41:33 -0300

    Update HTML.php

--- src/HTML.php
+++ src/HTML.php
@@ -2,50 +2,75 @@
 
 namespace PauloLeo\LaravelQJS;
 
+use PauloLeo\LaravelQJS\QJS;
+
 class HTML
 {
-    private function header($data)
-    {
-        $row = "<tr>";
-        if (count($data) > 0) {
-            $data = $data[0];
-            $data = (array) $data;
-            $data = array_keys($data);
-            foreach ($data as $col) {
-                $row .= "<th>{$col}</th>";
-            }
-            $row .= "</tr>";
-        }
-        return $row;
-    }
-
     private function body($data)
     {
-        if (count($data) > 0) {
-            $row = '';
-            $keys = $data[0];
-            $keys = (array) $keys;
-            $keys = array_keys($keys);
-            foreach ($data as $col) {
-                $row .= "<tr>";
-                $item = (array) $col;
-                foreach ($keys as $key) {
-                    $row .= "<td>{$item[$key]}</td>";
-                }
-                $row .= "</tr>";
-            }
-            return $row;
+        $qjs = new QJS;
+        if (!$data->query) {
+            return $this->rows($data);
         } else {
-            return '';
+            return $this->loopRows($data);
         }
+    }
+
+    private function adapterData($data)
+    {
+        $result = array();
+        $i = 0;
+        foreach ($data as $rows) {
+            foreach ($rows as $row) {
+                $result[$i][] = $row;
+            }
+            $i++;
+        }
+        return $result;
+    }
+
+    private function rows($data)
+    {
+        $table = "";
+        $query = $data->query;
+        $data = $data->data;
+
+        if (!$query) {
+            $data = $this->adapterData($data);
+        }
+
+        if (!is_array($data[0])) {
+            $data = [$data];
+        }
+
+        foreach ($data as $rows) {
+            $table .= "<tr>";
+            foreach ($rows as $row) {
+                $table .= "<td>{$row}</td>";
+            }
+            $table .= "<tr>";
+        }
+        return $table;
+    }
+
+    private function loopRows($data)
+    {
+        $table = "";
+        $items = $data->data;
+        foreach ($items as $key=>$value) {
+            $value = (object) ['data'=>$value,'query'=>true];
+            $table .= $this->rows($value);
+
+        }
+        return $table;
     }
 
     public function render($data, $name = 'report-system')
     {
         header('Content-Type: text/html');
         header('Cache-Control: no-cache');
-        
-        echo "<table><thead>{$this->header($data)}</thead><tbody>{$this->body($data)}</tbody></table>";
+
+        echo "<table border='1'>{$this->body($data)}</tbody></table>";
 
         exit();
     }
