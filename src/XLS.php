4ba5101662c00b0e7fc55a2d0c0e23a16787d09d
Paulo Leonardo <39014913+paulo-leo@users.noreply.github.com>
2024-04-25 16:39:37 -0300

    Update XLS.php

--- src/XLS.php
+++ src/XLS.php
@@ -2,53 +2,81 @@
 
 namespace PauloLeo\LaravelQJS;
 
+use PauloLeo\LaravelQJS\QJS;
+
 class XLS
 {
-    private function header($data)
-    {
-        $row = [];
-        if (count($data) > 0) {
-            $data = $data[0];
-            $data = (array) $data;
-            $data = array_keys($data);
-            foreach ($data as $col) {
-                $row[] = $col;
-            }
-            $row = implode("\t", $row) . "\n";
-        }
-        return $row;
-    }
-
     private function body($data)
     {
-        if (count($data) > 0) {
-            $body = [];
-            $keys = $data[0];
-            $keys = (array) $keys;
-            $keys = array_keys($keys);
-            foreach ($data as $col) {
-                $row = [];
-                $item = (array) $col;
-                foreach ($keys as $key) {
-                    $row[] = $item[$key];
-                }
-                $body[] = implode("\t", $row);
-            }
-            return implode("\n", $body);
+        $qjs = new QJS;
+        if (!$data->query) {
+            return $this->rows($data);
         } else {
-            return '';
+            return $this->loopRows($data);
         }
     }
 
-    public function render($data,$name='report-system')
+    private function adapterData($data)
+    {
+        $result = array();
+        $i = 0;
+        foreach ($data as $rows) {
+            foreach ($rows as $row) {
+                $result[$i][] = $row;
+            }
+            $i++;
+        }
+        return $result;
+    }
+
+    private function rows($data)
+    {
+        $table = [];
+        $query = $data->query;
+        $data = $data->data;
+
+        if (!$query) {
+            $data = $this->adapterData($data);
+        }
+
+        if (!is_array($data[0])) {
+            $data = [$data];
+        }
+
+        foreach ($data as $rows) {
+            $xls = [];
+            foreach ($rows as $row) {
+                $xls[] = $row;
+            }
+            $table[] = implode("\t", $xls);
+        }
+        return implode("\n", $table);
+    }
+
+    private function loopRows($data)
+    {
+        $table =[];
+        $items = $data->data;
+        foreach ($items as $key => $value) {
+            $value = (object) ['data' => $value, 'query' => true];
+            $table[] = $this->rows($value);
+
+        }
+        return implode("\n", $table);
+    }
+
+    public function render($data, $name = 'report-system')
     {
         header('Content-Type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename="'.$name.'.xls"');
+        header('Content-Disposition: attachment;filename="' . $name . '.xls"');
         header('Cache-Control: max-age=0');
 
-        echo $this->header($data);
-        echo $this->body($data);
-
+        try {
+            echo $this->body($data);
+        } catch (\Exception $e) {
+            echo "\tCode\tMessage\n";
+            echo "\t{$e->getCode()}\t{$e->getMessage()}";
+        }
         exit();
     }
 }
